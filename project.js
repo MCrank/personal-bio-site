4445349c81ed75832e3c62363b99533a2edcf644
MCrank <marco_mcse@hotmail.com>
2018-08-29 20:18:31 -0500

    Added boiler plate docs and basic looping through projects

---
+++ project.js
@@ -0,0 +1,49 @@
+const projects = [
+  {
+    title: 'Cool Project',
+    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
+    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed:
+      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: true,
+    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux'
+  },
+  {
+    title: 'Cool Project',
+    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
+    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed:
+      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: false,
+    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux'
+  }
+];
+
+const printToDom = (stringToPrint, divId) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = stringToPrint;
+};
+
+const createProjectCards = () => {
+  let newString = '';
+  for (let i = 0; i < projects.length; i++) {
+    if (projects[i].available == true) {
+      newString += `<h2>${projects[i].title}</h2>`;
+      newString += `<img src="${projects[i].screenshot}"></>`;
+      newString += `<p>${projects[i].description}</p>`;
+      newString += `<p>${projects[i].technologiesUsed}</p>`;
+      newString += `<a href="${
+        projects[i].url
+      }" style="display:block">Project Site</a>`;
+      newString += `<a href="${
+        projects[i].githubUrl
+      }" style="display:block">GitHub Link</a>`;
+    }
+  }
+
+  printToDom(newString, 'projectsPage');
+};
+
+createProjectCards();
