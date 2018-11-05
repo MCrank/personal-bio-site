3b04d7811f2755b944c8f2cdb6134147aab0c559
MCrank <marco_mcse@hotmail.com>
2018-11-05 07:46:55 -0600

    Added background video, started hero section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import './index.scss';
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import bckgrndVideo from './img/clouds-small.mp4';
 import bio from './javascripts/components/bio';
 
 library.add(faTwitter, faGithub, faLinkedin);
+
+$('#video-bkgrnd').attr('src', bckgrndVideo);
 
 const initApp = () => {
   bio.getBioData();
