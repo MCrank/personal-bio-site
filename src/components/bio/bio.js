e92aa269beccaf970afe955b359ee0ea08160c96
MCrank <marco_mcse@hotmail.com>
2018-11-19 18:07:08 -0600

    Bio WIP

--- src/components/bio/bio.js
+++ src/components/bio/bio.js
@@ -9,12 +9,10 @@ const bioStringBuilder = () => {
     <h1 class="display-4 text-white">Who Am I</h1>
     <hr class="my-4 border border-white" />
     <p class="lead">
-      This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
-      featured content or information.
+      Growing up as an "Army brat", I had the pleasure of staying in various locations around the world; finally settling in Clarksville, TN after my Dad's retirement from the Army.
     </p>
     <p class="lead">
-      This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
-      featured content or information.
+      I started my love of computers with a Commodore VIC-20 which continued on to Apple IIe in middle school, followed by a Commodore 64 through high school. Upon graduation from high school I joined the Navy as a Fire Control Technician aboard a nuclear powered attack submarine.
     </p>
     <p class="lead">
       This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
