c48a080cfaa68555bb2a329c9094d650b39fc232
MCrank <marco_mcse@hotmail.com>
2019-01-01 18:56:54 -0600

    Connect section working

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,26 +6,34 @@ const projectBuilder = (projects) => {
   let projDomString = '';
   projDomString += `
   <h1>Projects</h1>
+  <div class="d-flex justify-content-md-around flex-wrap">
   `;
   projects.forEach((project) => {
     projDomString += `
-  <div class="card mb-3" style="width: 50rem;">
-    <img src="${project.screenshot}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h4 class="card-title font-weight-bold text-white text-center">${project.title}</h4>
-      <hr class="border border-white" />
-      <h6 class="card-subtitle mb-2 text text-white"><strong><u>Technologies Used</u>: </strong> ${
+
+      <div class="card mb-3" style="width: 50rem;">
+        <img src="${project.screenshot}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h4 class="card-title font-weight-bold text-white text-center">${project.title}</h4>
+          <hr class="border border-white" />
+          <h6 class="card-subtitle mb-2 text text-white"><strong><u>Technologies Used</u>: </strong> ${
   project.technologiesUsed
 }</h6>
-      <p class="card-text text-white">${project.description}</p>
-      <div class="card-footer">
-        <a href="${project.githubUrl}" target="_blank" class="btn btn-primary">View on Github</a>
-        <a href="${project.url}" target="_blank" class="btn btn-primary">View Live Demo</a>
+          <p class="card-text text-white">${project.description}</p>
+          <div class="card-footer">
+            <a href="${
+  project.githubUrl
+}" target="_blank" class="btn btn-primary">View on Github</a>
+            <a href="${project.url}" target="_blank" class="btn btn-primary">View Live Demo</a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+
   `;
   });
+  projDomString += `
+  </div>
+  `;
   $('#my-projects').html(projDomString);
 };
 
